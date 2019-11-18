64fcf0305a08ff159a18f3280e01813c59a56f1c
dongHyun <dhmoon@naver.com>
2019-11-18 19:55:58 +0900

    React first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PhoneForm from './components/PhoneForm';
+
+function App() {
+    return (
+        <div>
+           <PhoneForm />
+        </div>
+    );
+}
+
+export default App;
